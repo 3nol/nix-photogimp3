6204d018093e1fca93e04b1f493c65f44d2f658c
Leon Wenzler <leon.wenzler@uni.kn>
2025-07-21 23:06:15 +0200

    feat: un-sandbox by removing NixPak

--- photogimp3/wrapper.nix
+++ photogimp3/wrapper.nix
@@ -1,28 +1,27 @@
 {
   pkgs,
-  lib,
-  gimp,
-  photo-gimp-files,
+  gimp3-exe,
+  photogimp3-files,
 }:
 pkgs.writeShellApplication {
-  name = "gimp";
-  text =
-    # bash
-    ''
-      if [ ! -f "$HOME/.config/GIMP/3.0/.photo-gimp-installed" ]; then
-        echo "Creating GIMP Configuration"
-        ${lib.getExe gimp} -i --quit > /dev/null
-        echo "Adding PhotoGIMP Configuration"
-        cp --backup=simple --suffix=.bac -r ${photo-gimp-files}/.config/GIMP/3.0/. "$HOME/.config/GIMP/3.0"
+  name = "photogimp3-wrapper";
 
-        find "$HOME/.config/GIMP/3.0" -type f -exec chmod 644 {} +
-        find "$HOME/.config/GIMP/3.0" -type d -exec chmod 755 {} +
+  text = ''
+    if [ ! -f "$HOME/.config/GIMP/3.0/.photogimp3" ]; then
+      echo "Creating GIMP Configuration..."
+      ${gimp3-exe} -i --quit > /dev/null
 
-        echo "Configuring finished"
-        touch "$HOME/.config/GIMP/3.0/.photo-gimp-installed"
-      fi
-      ${lib.getExe gimp}
-    '';
+      echo "Adding PhotoGIMP Configuration..."
+      cp --backup=simple --suffix=.backup -r ${photogimp3-files}/.config/GIMP/3.0/. "$HOME/.config/GIMP/3.0"
+      find "$HOME/.config/GIMP/3.0" -type f -exec chmod 644 {} +
+      find "$HOME/.config/GIMP/3.0" -type d -exec chmod 755 {} +
+
+      echo "Configuration done."
+      touch "$HOME/.config/GIMP/3.0/.photogimp3"
+    fi
+    ${gimp3-exe}
+  '';
+
   runtimeInputs = with pkgs; [
     coreutils
     findutils
