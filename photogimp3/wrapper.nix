6d57f35bf9a3f6b599e4fe4d2e07d76a049984a4
Leon Wenzler <leon.wenzler@uni.kn>
2025-07-21 23:38:48 +0200

    fix: source XDG_CONFIG_HOME in wrapper

--- photogimp3/wrapper.nix
+++ photogimp3/wrapper.nix
@@ -4,27 +4,29 @@
   photogimp3-files,
 }:
 pkgs.writeShellApplication {
-  name = "photogimp3-wrapper";
-
-  text = ''
-    if [ ! -f "$HOME/.config/GIMP/3.0/.photogimp3" ]; then
-      echo "Creating GIMP Configuration..."
-      ${gimp3-exe} -i --quit > /dev/null
-
-      echo "Adding PhotoGIMP Configuration..."
-      cp --backup=simple --suffix=.backup -r ${photogimp3-files}/.config/GIMP/3.0/. "$HOME/.config/GIMP/3.0"
-      find "$HOME/.config/GIMP/3.0" -type f -exec chmod 644 {} +
-      find "$HOME/.config/GIMP/3.0" -type d -exec chmod 755 {} +
-
-      echo "Configuration done."
-      touch "$HOME/.config/GIMP/3.0/.photogimp3"
-    fi
-    ${gimp3-exe}
-  '';
-
+  name = "photo${pkgs.gimp3.pname}-wrapper";
   runtimeInputs = with pkgs; [
     coreutils
     findutils
     bash
   ];
+
+  text = ''
+    cfg="''${XDG_CONFIG_HOME:-$HOME/.config}/GIMP/3.0"
+
+    if [ ! -f "$cfg/.photogimp3" ]; then
+      echo "Creating GIMP configuration..."
+      ${gimp3-exe} -i --quit > /dev/null
+
+      echo "Overwriting with PhotoGIMP configuration..."
+      cp --backup=simple --suffix=.backup -r ${photogimp3-files}/.config/GIMP/3.0/. "$cfg"
+      find "$cfg" -type f -exec chmod 644 {} +
+      find "$cfg" -type d -exec chmod 755 {} +
+
+      touch "$cfg/.photogimp3"
+      echo "Done."
+    fi
+
+    exec ${gimp3-exe} "$@"
+  '';
 }
