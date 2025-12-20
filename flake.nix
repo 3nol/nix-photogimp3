8fcba74caac50713b1ac3bbbccb6baae59e623be
Ben Mayer <info@leocodes.net>
2025-12-20 17:50:11 +0100

    nix: update inputs and nixpak

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpak = {
-      url = "github:Keksgesicht/nixpak/dbus-instance";
+      url = "github:nixpak/nixpak";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
